192206dcacb1ca075204ff1e8246e19098372b11
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-04-03 07:31:53 -0300

    add: trivial resolver

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -32,6 +32,11 @@ const typeDefs = `
 `;
 
 const resolvers = {
+    User: {
+        id: (user) => user.id,
+        name: (user) => user.name,
+        email: (user) => user.email
+    },
     Query: {
         allUsers: () => users
     },
@@ -41,7 +46,7 @@ const resolvers = {
             users.push(newUser);
             return newUser;
         }
-    }
+    },
 };
 
 export default makeExecutableSchema({ typeDefs, resolvers });
